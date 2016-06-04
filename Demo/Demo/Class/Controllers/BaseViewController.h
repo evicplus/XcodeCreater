1255b25777b1293eb7f0dbca97dac6ab639eb020
Evic Chen <everblacks@163.com>
2016-06-04 22:22:33 +0800

    添加Demo 工程 和 创建的shell

---
+++ Demo/Demo/Class/Controllers/BaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  BaseViewController.h
+//  Demo
+//
+//  Created by Evic Chen on 6/4/16.
+//  Copyright © 2016 Evic Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseViewController : UIViewController
+
+@end
---
+++ Demo/Demo/Class/Controllers/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  Demo
+//
+//  Created by Evic Chen on 6/4/16.
+//  Copyright © 2016 Evic Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "BaseViewController.h"
+
+@interface ViewController : BaseViewController
+
+
+@end
+
